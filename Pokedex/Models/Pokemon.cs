810f9cb045a79b8fb444882335c658c2345ce9e5
LuksCod <Lucas.MTheodosio@gmail.com>
2025-08-30 02:58:42 -0300

    Atualização do Projeto em sala

---
+++ Pokedex/Models/Pokemon.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pokedex.Models;
+[Table("Pokemon")]
+public class Pokemon
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public uint Numero { get; set; }
+
+    [Required(ErrorMessage = "Por favor, informe a Região")]
+    public uint RegiaoId { get; set; }
+    [ForeignKey("RegiaoId")]
+    public Regiao Regiao { get; set; }
+
+    [Required(ErrorMessage = "Por favor, informe o Genêro")]
+
+    public uint GeneroId { get; set; }
+    [ForeignKey("GeneroId")]
+    public Genero Genero { get; set; }
+
+    [Required(ErrorMessage = "Por favor, informe o Nome")]
+
+    public string Nome { get; set; }
+
+    [StringLength(1000)]
+
+    public string Descricao { get; set; }
+
+    [Column(TypeName = "double(5,2)")]
+    [Required(ErrorMessage ="Por favor, informe a altura")]
+    public string Altura { get; set; }
+
+    [Column(TypeName = "double(7,3)")]
+    [Required(ErrorMessage ="Por favor, informe o peso")]
+    public string Peso { get; set; }
+
+    [StringLength(200)]
+    public string Imagem { get; set; }
+
+    [StringLength(400)]
+    public string Animacao { get; set; }
+
+    public ICollection<PokemonTipo> Tipos { get; set; }
+}
